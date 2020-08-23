208dfb777eefe6b90f0c313450768692f59e0fe1
shinevol53 <shinevol@gmail.com>
2020-08-23 23:52:59 +0900

    <note>  java_operator

---
+++ Java/Data_and_Operator/Datatype.java
@@ -0,0 +1,18 @@
+public class Datatype{
+	public static void main(String[] args) {
+		System.out.println(6); // Number
+		System.out.println("six"); // String
+		System.out.println("6"); // String 6
+		
+		System.out.println(6+6); // 12
+		System.out.println("6"+"6"); // 66
+		
+		System.out.println(6*6); // 36
+//		System.out.println("6"*"6"); // Error
+		System.out.println("1111".length()); // 4
+		System.out.println(1111.length()); // Error
+// Type에 어울리는 연산방법이 있기 때문에 programming에서는 엄격히 구분
+// 내가 만드는 시스템에는 어떤 데이터 타입이 있는가?
+// 어떤 연산방법들이 존재하나?
+	}
+}
---
+++ Java/Data_and_Operator/StringApp.java
@@ -0,0 +1,21 @@
+
+public class StringApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World"); // String
+		//System.out.println('Hello World'); // Character
+		System.out.println("H"); // String
+		System.out.println('H'); // Character
+		
+		System.out.println("Hello"
+				+ "World");
+		// new line
+		System.out.println("Hello \nWorld");
+		
+		// escape: 일시적으로 문자를 해방
+		System.out.println("Hello \"World\""); // Hello "World"
+		
+	}
+
+}
